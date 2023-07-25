caf1b47735b3307e3951cd8ccfe5f11f005fada3
ccostarod <ccostarodrigoo@gmail.com>
2023-07-25 19:07:13 -0300

    New Exercise

--- src/ex2/application/exercicio_2.java
+++ src/ex2/application/exercicio_2.java
@@ -1,4 +1,30 @@
 package ex2.application;
+import ex2.entities.Employee;
+
+
+import java.util.Locale;
+import java.util.Scanner;
 
 public class exercicio_2 {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Employee user;
+        user = new Employee();
+        System.out.print("Name: ");
+        user.name = sc.nextLine();
+        System.out.print("Gross Salary: ");
+        user.GrossSalary = sc.nextDouble();
+        System.out.print("Tax: ");
+        user.Tax = sc.nextDouble();
+
+        user.employee();
+        System.out.print("Which percentage to increase salary? ");
+
+        double percentage = sc.nextDouble();
+
+        user.update(percentage);
+
+    }
 }
--- src/ex2/entities/Employee.java
+++ src/ex2/entities/Employee.java
@@ -1,4 +1,16 @@
 package ex2.entities;
 
 public class Employee {
+    public String name;
+    public double GrossSalary;
+    public double Tax;
+
+    public void employee(){
+        System.out.printf("Employee: %s, $ %.2f\n", name, GrossSalary - Tax);
+    }
+
+    public void update(double porcentage){
+        double salary_update = (GrossSalary - Tax) + GrossSalary * porcentage/100;
+        System.out.printf("Update Data: %s, $ %.2f", name, salary_update);
+    }
 }
